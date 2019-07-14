df4c545d7aeea91b58fe81dea8f3e486c526339d
Qun Song <songqun001@gmail.com>
2019-07-15 00:53:34 +0800

    conv.cpp & conv.hpp: Add some helper functions

--- include/conv.hpp
+++ include/conv.hpp
@@ -1,31 +1,62 @@
+enum ConvAlg
+{
+  CONV_1X1S1P0,
+  CONV_WINO_TWO_STEP,
+  CONV_WINO_ONE_STEP,
+  CONV_IM2COL,
+  CONV_NOT_MATCH,
+};
+
+
 /*
- * n: num_batch, ic: input_channel, ih: input_height, iw: input_width, oc: output_channel, s: stride, p: padding
+ * nb: num_batch, ic: input_channel, ih: input_height, iw: input_width, oc: output_channel, s: stride, p: padding, oh: output_height, ow: output_width
  * input data layout: NCHWc4
  * output data layout: NOHWo4
  * weight data layout: it depends
  * computing kernel: sgemm_8x8
+ * Assume ic and oc have been divided by 4
  */
 void conv(const float *input, const float *weight, float *output, const float *bias,
-          int num, int ic, int ih, int iw, int oc, int fh, int fw, int s, int p);
+          int nb, int ic, int ih, int iw, int oc, int oh, int ow,
+          int fh, int fw, int s, int p, float *buf, ConvAlg alg);
 
 
 /*
  * fh=fw=1, s=1, p=0. It is a GEMM
  * do input packing when first meet in computing. NCHWhw8c4 => NHWCc4hw8 (NCHWc4 => NHWChw8)
  * weight data layout: OCo8 (fh=fw=1)
+ * Assume ic and oc have been divided by 4
  */
 void conv1x1s1p0(const float *input, const float *weight, float *output, const float *bias,
-                 int num, int ic, int ih, int iw, int oc, int fh, int fw, int s, int p);
+                 int nb, int ic, int ih, int iw, int oc, int oh, int ow,
+                 int fh, int fw, int s, int p, float *buf);
 
 
 /*
  * winograd, F(4x4, 6x6)
+ * two step: store the whole in_tm, then do dot prod, then transform the whole out_tm
+ * input trans data layout: NCHWc4 => N*(6*6)*HW*C*hw8
+ * output trans data layout: N*(6*6)*HW*O*o4 => NOHWo4
+ * weight data layout: N*(6*6)*O*T*C*o8 (T = oh/4*ow/4, tiles: nb of 6x6 block on one channel)
+ * Assume ic and oc have been divided by 4
+ */
+void conv3x3s1p1_wino_two_step(const float *input, const float *weight, float *output, const float *bias,
+                               int nb, int ic, int ih, int iw, int oc, int oh, int ow,
+                               int fh, int fw, int s, int p, float *buf);
+
+
+/*
+ * winograd, F(4x4, 6x6)
+ * one step: do partly in_tm, then do dot prod on them, and transform them back
+ * maybe better than two step for all data loaded into cache only once if blocked properly
  * input trans data layout: NCHWc4 => N*(6*6)*HW*C*hw8
  * output trans data layout: N*(6*6)*HW*O*o4 => NOHWo4
  * weight data layout: N*(6*6)*O*T*C*o8 (T = oh/4*ow/4, tiles: num of 6x6 block on one channel)
+ * Assume ic and oc have been divided by 4
  */
-void conv3x3s1p1_wino(const float *input, const float *weight, float *output, const float *bias,
-                      int num, int ic, int ih, int iw, int oc, int fh, int fw, int s, int p);
+void conv3x3s1p1_wino_one_step(const float *input, const float *weight, float *output, const float *bias,
+                               int nb, int ic, int ih, int iw, int oc, int oh, int ow,
+                               int fh, int fw, int s, int p, float *buf);
 
 
 /*
@@ -33,23 +64,27 @@ void conv3x3s1p1_wino(const float *input, const float *weight, float *output, co
  * only open 8*fh*fw*C buffer to do im2col when first meet in computing instead of HW*fh*fw*C for whole input
  * do input packing when first meet. NCHWhw8c4 => NHWCc4hw8 (NCHWc4 => NHWChw8)
  * weight data layout: OHWCo8
+ * Assume ic and oc have been divided by 4
  */
 void conv_im2col(const float *input, const float *weight, float *output, const float *bias,
-                 int num, int ic, int ih, int iw, int oc, int fh, int fw, int s, int p);
+                 int nb, int ic, int ih, int iw, int oc, int oh, int ow,
+                 int fh, int fw, int s, int p, float *buf);
 
 
 /*
  * maybe not implement
  * weight data layout: OCHWo8
+ * Assume ic and oc have been divided by 4
  */
 void conv_direct(const float *input, const float *weight, float *output, const float *bias,
-                 int num, int ic, int ih, int iw, int oc, int s, int p);
+                 int nb, int ic, int ih, int iw, int oc, int oh, int ow,
+                 int fh, int fw, int s, int p, float *buf);
 
 
 /*
  * get oh and ow
  */
-void get_output_hw(int ih, int iw, int s, int p, int *oh, int *ow);
+void get_output_hw(int ih, int iw, int fh, int fw, int s, int p, int *oh, int *ow);
 
 
 /*
@@ -98,3 +133,18 @@ void input_trans_wino(const float *input, float *input_tm);
  * output trans data layout: N*(6*6)*HW*O*o4 => NOHWo4
  */
 void output_trans_wino(const float *output, float *output_tm);
+
+
+/*
+ * infer conv algorithm
+ * Assume ic and oc have been divided by 4
+ */
+void infer_conv_alg(int nb, int ic, int ih, int iw, int oh, int ow, int oc,
+                    int fh, int fw, int s, int p, ConvAlg *alg);
+
+
+/*
+ * compute buffer size for specific conv algorithm
+ * Assume ic and oc have been divided by 4
+ */
+void conv_buffer_size(int nb, int ic, int ih, int iw, int oh, int ow, int oc, int fh, int fw, int s, int p, ConvAlg alg, int *bytes);
---
+++ src/conv.cpp
@@ -0,0 +1,98 @@
+#include "../include/conv.hpp"
+#include <string.h>
+
+
+// More description see header for detail.
+
+
+void conv3x3s1p1_wino_two_step(const float *input, const float *weight, float *output, const float *bias,
+                 int nb, int ic, int ih, int iw, int oh, int ow, int oc, int fh, int fw, int s, int p, float *buf)
+{
+
+}
+
+
+void conv1x1s1p0(const float *input, const float *weight, float *output, const float *bias,
+                 int nb, int ic, int ih, int iw, int oh, int ow, int oc, int fh, int fw, int s, int p, float *buf)
+{
+
+}
+
+
+void conv_im2col(const float *input, const float *weight, float *output, const float *bias,
+                 int nb, int ic, int ih, int iw, int oh, int ow, int oc, int fh, int fw, int s, int p, float *buf)
+{
+
+}
+
+
+void get_output_hw(int ih, int iw, int fh, int fw, int s, int p, int *oh, int *ow)
+{
+  // ignore remainder if it appears divided by s, like what Caffe does.
+  *oh = (ih + 2*p - fh) / s + 1;
+  *ow = (iw + 2*p - fw) / s + 1;
+}
+
+
+void infer_conv_alg(int nb, int ic, int ih, int iw, int oh, int ow, int oc, int fh, int fw, int s, int p, ConvAlg *alg)
+{
+  if (fh!=fw || (oc%2)!=0) {
+    *alg = CONV_NOT_MATCH;
+  }
+
+  // TODO make implementations more general when there are remainders.
+  if (fh==1 && fw==1 && s==1 && p==0
+      && (oh*ow)%8==0) {
+    *alg = CONV_1X1S1P0;
+  } else if (fh==3 && fw==3 && s==1 && p==1
+             && oh%4==0 && ow%4==0 && (oh*ow)%128==0) {
+    *alg = CONV_WINO_TWO_STEP;
+  } else if (oh*ow%8==0){
+    *alg = CONV_IM2COL;
+  } else {
+    *alg = CONV_NOT_MATCH;
+  }
+}
+
+
+void conv_buffer_size(int nb, int ic, int ih, int iw, int oh, int ow, int oc, int fh, int fw, int s, int p, ConvAlg alg, int *bytes)
+{
+  switch(alg) {
+    case CONV_1X1S1P0:
+      // in_pack
+      *bytes = (ic*4)*8;
+      break;
+    case CONV_WINO_TWO_STEP:
+      *bytes = 0; //TODO
+      break;
+    case CONV_IM2COL: {
+      // padding + in_pack
+      int ih_pad = ih + 2*p;
+      int iw_pad = iw + 2*p;
+      *bytes = ic*ih_pad*iw_pad*4 + fh*fw*(ic*4)*8;
+      break;
+    }
+    default:
+      break;
+  }
+  *bytes *= sizeof(float);
+}
+
+
+void conv(const float *input, const float *weight, float *output, const float *bias,
+          int nb, int ic, int ih, int iw, int oc, int oh, int ow, int fh, int fw, int s, int p, float *buf, ConvAlg alg)
+{
+  switch(alg) {
+    case CONV_1X1S1P0:
+      conv1x1s1p0(input, weight, output, bias, nb, ic, ih, iw, oc, oh, ow, fh, fw, s, p, buf);
+      break;
+    case CONV_WINO_TWO_STEP:
+      conv3x3s1p1_wino_two_step(input, weight, output, bias, nb, ic, ih, iw, oc, oh, ow, fh, fw, s, p, buf);
+      break;
+    case CONV_IM2COL:
+      conv_im2col(input, weight, output, bias, nb, ic, ih, iw, oc, oh, ow, fh, fw, s, p, buf);
+      break;
+    default:
+      break;
+  }
+}
